478e2229642fc33331f98f95b9498923456f3d85
Jose Flavio de Souza Dias Junior <contato@joseflavio.com>
2017-04-23 16:20:46 -0300

    SID e LID: prioridade dos cookies

--- iperoxo-web/js/aplicacao/aplicacao.js
+++ iperoxo-web/js/aplicacao/aplicacao.js
@@ -1,16 +1,24 @@
 //--------------------------------------------------------------------------
 
 // Copyright (C) 2016 José Flávio de Souza Dias Júnior
-// Ipê-roxo 1.0-A12 - <http://www.joseflavio.com/iperoxo/>.
+// Ipê-roxo 1.0-A13 - <http://www.joseflavio.com/iperoxo/>.
 
 //--------------------------------------------------------------------------
 
 // Comportamento inicial da aplicação.
 $(document).ready(function(){
-    setSID( gerarID() );
-    setLinguagem( navigator.language );
+	
+	sid = getCookie( "sid" );
+	if( sid == "" ) sid = gerarID();
+	setSID( sid );
+
+	lid = getCookie( "lid" );
+    if( lid == "" ) lid = navigator.language;
+    setLinguagem( lid );
+    
     setCookie( "cookieTeste", "Ipê-roxo ~ Teste de Cookie!" );
     abrirTela( "html/tela.html" );
+
 });
 
 //--------------------------------------------------------------------------
